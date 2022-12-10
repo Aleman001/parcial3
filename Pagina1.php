d82805eb6b095bd92699d027accfb1a083623ea4
Aleman001 <60446447+Aleman001@users.noreply.github.com>
2022-12-09 21:28:31 -0600

    Create Pagina1.php

---
+++ Pagina1.php
@@ -0,0 +1,10 @@
+Pagina1.php
+<form method="post"
+action="pagina2.php">
+Por favor,envía la newsletter a:<br/>
+Tu nombre:<input type="text"
+name="nombre" /><br/>
+Tu apellido:<input type="text"
+name="apellido" /><br/>
+<input type="submit" value="Enviar formulario" />
+</form>
